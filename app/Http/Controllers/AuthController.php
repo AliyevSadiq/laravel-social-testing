7f06471197699e446d432ef57f4901117ef0bfc2
AliyevSadiq <sadiq.eliyev1995@bk.ru>
2021-02-01 15:57:16 +0400

    initial

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class AuthController extends Controller
+{
+
+
+    public function redirectToProvider(){
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleProviderCallBack(){
+        $user=Socialite::driver('google')->user();
+
+        dump($user);
+    }
+}
